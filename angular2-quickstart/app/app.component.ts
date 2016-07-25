4edc81de1c04045476646dd0b72bdb6719c2f7a5
Álvaro Castellano <alvaro.castellano.vela@gmail.com>
2016-07-25 11:57:28 +0200

    Reviewing the tutorial

--- angular2-quickstart/app/app.component.ts
+++ angular2-quickstart/app/app.component.ts
@@ -1,7 +1,53 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `<h1>{{title}}</h1>
+  <ul>
+               <p>There are {{totalCarParts()}} total parts in stock.</p>
+               <li *ngFor="let carPart of carParts">
+                       <h2>{{carPart.name | uppercase }}</h2>
+                       <p>{{carPart.description}}</p>
+                       <p>{{carPart.price | currency:'USD':true}}
+                       <p *ngIf="carPart.inStock > 0" >{{carPart.inStock}} in Stock</p>
+                       <p *ngIf="carPart.inStock === 0">Out of Stock</p> 
+               </li>
+               </ul>`
 })
-export class AppComponent { }
+export class AppComponent {
+        title = "Ultra Racing" ;
+        carParts = [{
+                "id" : 1,
+                "name" : "Super Tires",
+                "description": "These tires are the very best",
+                "inStock" : 5,
+                "price" : 4.99
+        },
+        {
+                "id" : 2,
+                "name" : "Reinforced Shocks",
+                "description" : "Shocks made from kryptonite",
+                "inStock" : 4,
+                "price" : 9.99
+                },
+        {
+                "id": 3,
+                "name" : "Padded Seats",
+                "description" : "Super padded seats",
+                "inStock" : 0,
+                "price" : 10.99
+        }
+        ]
+
+        totalCarParts()
+        {
+        //let sum = 0;
+
+        //for (let carPart of this.carParts)
+        //{
+        //        sum += carPart.inStock;
+        //        }
+        //        return sum;
+        return this.carParts.reduce((prev,current) => prev + current.inStock, 0 );
+        }
+}
 
